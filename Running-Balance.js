b0a398055e4104356b41c5a3321e3aa2ebf28d40
Mostafa Zamani <119905696+mstfazmni@users.noreply.github.com>
2025-09-25 18:40:39 -0600

    start code for running balance

---
+++ Running-Balance.js
@@ -0,0 +1,15 @@
+// You’re given an array of transactions.
+// Each transaction is either a "deposit" or a "withdrawal", with an amount.
+// I want you to process them in order and return the running balance after each transaction.
+
+const transactions = [
+    { type: "deposit", amount: 100 },
+    { type: "withdrawal", amount: 30 },
+    { type: "deposit", amount: 50 }
+]
+
+function runningBalanceCalculator () {
+    
+}
+
+runningBalanceCalculator();
