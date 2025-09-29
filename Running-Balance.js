8b863cdbf2cef647964e47e679a6e0a60243b7dd
Mostafa Zamani <119905696+mstfazmni@users.noreply.github.com>
2025-09-28 21:05:28 -0600

    add comments

--- Running-Balance.js
+++ Running-Balance.js
@@ -13,15 +13,19 @@ function runningBalanceCalculator (transactions) {
     const finalArr = [];
 
     transactions.forEach((t) => {
-        // console.log(t)
+        // console.log(t) to test
+        // if deposti add to balance
         if (t.type === "deposit") {
             balance += t.amount;
+            // if withdrawal subtract from balance
         } else if (t.type === "withdrawal") {
             balance -= t.amount;
+            // else throw error
         } else {
             throw new Error(`Unknown transaction type: ${t.type}`);
         }
 
+        // no overdraw is permitted
         if (balance < 0) {
             throw new Error("Balance cannot be smaller than 0!")
         }
