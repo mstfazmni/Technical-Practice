837ca3599b231e8b502988680af3a1afe52baf19
Mostafa Zamani <119905696+mstfazmni@users.noreply.github.com>
2025-11-17 16:48:49 -0700

    create sample data file and SearchFilter component

---
+++ Task-001/my-app/src/data/data.ts
@@ -0,0 +1,16 @@
+// Define the Product type
+type Product = {
+    title: string,
+    price: number,
+    inStock: boolean   
+}
+
+// Create an array of products
+const products: Product[] = [
+    { title: "Laptop", price: 1200, inStock: true },
+    { title: "Headphones", price: 150, inStock: false },
+    { title: "Keyboard", price: 80, inStock: true },
+    { title: "Mouse", price: 10, inStock: false },
+    { title: "Notebook", price: 20, inStock: true },
+    { title: "Speaker", price: 40, inStock: false }
+]
