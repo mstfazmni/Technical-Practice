d060a561f2577511a1c59b66caf46e2cbf848fff
Mostafa Zamani <119905696+mstfazmni@users.noreply.github.com>
2025-11-18 17:15:08 -0700

    created enum for fixed types and status ad Transaction interface for typing my objects and a function that displays a Transaction variable typed by user

--- Bank-Transaction.ts
+++ Bank-Transaction.ts
@@ -1,8 +1,8 @@
 enum TransactionType {
-    Deposit = "Deposit",
-    Withdrawal = "Withdrawal",
-    Transfer = "Transfer",
-    Payment = "Payment"
+  Deposit = "Deposit",
+  Withdrawal = "Withdrawal",
+  Transfer = "Transfer",
+  Payment = "Payment"
 }
 
 enum TransactionStatus {
@@ -17,7 +17,24 @@ interface Transaction {
     amount: number,
     currency: string,
     status: TransactionStatus,
-    createdAt: Date,
+    createdAt?: Date,
     completedAt?: Date,
     notes?: string
 }
+
+
+
+function displayVar (param: Transaction) {
+    return `id: ${param.id} - type: ${param.type} - amount: ${param.amount} - currency: ${param.currency} - status: ${param.status}`;
+}
+
+
+const transaction1: Transaction = {
+    id: "Food",
+    type: TransactionType.Payment,
+    amount: 120,
+    currency: "Dollar",
+    status: TransactionStatus.Completed,
+}
+
+console.log(displayVar(transaction1));
